dccef551b4921a37e6717171e4066f5be8c984ec
Desarrollo Aplicaciones <karen.orozco.sg@gmail.com>
2022-03-24 23:39:50 -0600

    second commit

--- js/app.js
+++ js/app.js
@@ -1,22 +1,66 @@
-const fetchPokemon = () => {
-    const pokeInput = document.getElementById("pokeName");
-    let pokeName = pokeInput.value.toLowerCase();
+const fetchPokemon = (url) => {
 
-    const url = "https://pokeapi.co/api/v2/pokemon/"+pokeName;
-    
-    fetch(url).then((res) => {
-        if (res.status != "200"){
-            console.log(res);
+    fetch(url).then((result) =>{
+        var status = result.status;
+        if(result.status !== 200){
+            console.log(result.status)
             changePokeImage("./pokemon-sad.gif");
         }else{
-            return res.json();
+            console.log(result.status);
+            return result.json();
         }
     }).then((data) =>{
-        console.log(data);
-        let pokeImg = data.sprites.front_default;
-        console.log(pokeImg);
-        changePokeImage(pokeImg);
+        if(typeof data !== 'undefined'){
+            console.log(data.results);
+            // return data.results;
+            // this.getDataPokemon(data);
+            var typesPokemonList = Object.values(data.results); //Tu array de provincias
+            return addOptionsToTypesPokemonSelect(typesPokemonList);
+        }
     })
+}
+
+const getTypesPokemonList = () => {
+    const url = 'https://pokeapi.co/api/v2/type/';
+    return fetchPokemon(url);
+}
+
+function addOptionsToTypesPokemonSelect(typesPokemonList) {
+    var select = document.getElementById("typesPokemon"); //Seleccionamos el select
+    console.log(typesPokemonList);
+
+    for(var i=0; i < typesPokemonList.length -2; i++){ 
+        var option = document.createElement("option"); //Creamos la opcion
+        option.innerHTML = typesPokemonList[i].name; //Metemos el texto en la opción
+        option.value = typesPokemonList[i].url.substr(31,2).replace('/','');
+        select.appendChild(option); //Metemos la opción en el select
+    }
+}
+
+const searchPokemon = () => {
+    const pokeSearchType = document.getElementById("pokeSearchType");
+    const pokeInput = document.getElementById("pokeName");
+    let pokeValue = pokeInput.value.toLowerCase();
+    let url = "https://pokeapi.co/api/v2/";
+
+    switch(pokeSearchType){
+        case 'pokemon':
+            url += pokeSearchType + '/' + pokeValue;
+            break;
+        case 'type':
+            url += pokeSearchType + '/' + pokeValue;
+            break;
+
+        case 'ability':
+
+            break;
+
+        default:
+            
+            break;
+    }
+
+    return url;
 }
 
 const changePokeImage = (url) => {
@@ -24,10 +68,4 @@ const changePokeImage = (url) => {
     pokeInput.src = url;
 }
 
-// changePokeImage("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/7.png");
-
-const imprimir = () => {
-    const pokeInput = document.getElementById("pokeName");
-    let pokeName = pokeInput.value;
-    console.log("hola " + pokeName);
-}
+getTypesPokemonList();
